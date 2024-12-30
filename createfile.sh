cbfe5882e0b40776bd0da2ab0cfe0476d99ec4b0
ananyahegde <ananyahegde533@gmail.com>
2024-12-30 13:56:57 +0530

    all the files are being pushed

---
+++ createfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir /home/unique_12/Documents/shellScript/dir1
+
+touch /home/unique_12/Documents/shellScript/dir1/samplefile.txt
+
+echo "Working fine" >> /home/unique_12/Documents/shellScript/dir1/samplefile.txt
+
